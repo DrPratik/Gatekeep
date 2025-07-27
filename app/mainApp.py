f0f15b6c4e41b6079af96328ad4c627564c34b89
Pratik Dhore <41249340+DrPratik@users.noreply.github.com>
2025-07-27 22:29:07 +0530

    Update mainApp.py

--- app/mainApp.py
+++ app/mainApp.py
@@ -315,7 +315,7 @@ def annotate_from_url(url, threshold):
 app = connexion.App(__name__)
 # For compatibility we will make the API available both with and without a version basepath
 app.add_api('swag.yaml')
-app.add_api('swag.yaml', base_path='/1.0')
+app.add_api('swag.yaml', base_path='/gatekeep')
 
 if __name__ == '__main__':
     signal.signal(signal.SIGTERM, sigterm_handler)
